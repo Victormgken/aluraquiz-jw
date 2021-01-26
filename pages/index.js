77955c939f362e4e169341120f51bf85982df252
Victormgken <victormgken@gmail.com>
2021-01-26 15:45:56 -0300

    codigo final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
+import { delBasePath } from 'next/dist/next-server/lib/router/router'
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../source/components/Widget/';
+import QuizBackground from '../source/components/QuizBackground';
+import Footer from '../source/components/Footer';
+import GitHubCorner from '../source/components/GitHubCorner';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+const BackgroundImage = styled.div`
+   background-image: url(${db.bg});
+   flex: 1;
+   background-size: cover;
+   background-position: center;
+ `;
+
+export const QuizContainer = styled.div`
+width: 100%;
+max-width: 350px;
+padding-top: 45px;
+margin: auto 10%;
+@media screen and (max-width: 500px) {
+  margin: auto;
+  padding: 15px;
+}
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage= {db.bg} >
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+          <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem impsum dolor isit mesen...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer>
+
+        </Footer>
+        <GitHubCorner projectUrl=""/>
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
